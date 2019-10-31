6a2400560653a6561e6929ce728de8da2543832e
remi meullemeestre <remondo02@hotmail.com>
2019-10-31 15:09:17 +0100

    Templates

---
+++ content/themes/ocooking/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+     <div class="footer__text">oCooking par <a class="footer__text__author" href="#">oClock Nova</a>. La meilleure source pour se régaler.</div>
+     <div class="copyright">oCooking 2019</div>
+   </footer>
+   <?php wp_footer(); ?>
+ </body>
+ </html>
---
+++ content/themes/ocooking/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php wp_head(); ?>
+</head>
+<body class="home">
+  <header class="header">
+    <div class="header__container header__container--start">
+      <a class="logo" href="#">oCooking</a>
+      <nav class="menu">
+        <ul class="menu__list">
+          <li class="menu__list__list-item">
+            <a class="menu__list__list-item__link" href="#">Au hasard</a>
+          </li>
+          <li class="menu__list__list-item">
+            <a href="#">Recettes</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div class="header__container header__container--end">
+      <nav class="menu">
+        <ul class="menu__list">
+          <li class="menu__list__list-item">
+            <a class="menu__list__list-item__link" href="#"><i class="fa fa-facebook"></i></a>
+          </li>
+          <li class="menu__list__list-item">
+            <a href="#"><i class="fa fa-twitter"></i></a>
+          </li>
+        </ul>
+      </nav>
+      <a class="ui-button" href="#"><i class="fa fa-bars"></i></a>
+    </div>
+  </header>
