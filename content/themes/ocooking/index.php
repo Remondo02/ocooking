4cd53894e6d7d02a8549bf6e6bf199248ab374c2
remi meullemeestre <remondo02@hotmail.com>
2019-10-31 15:09:17 +0100

    Templates

--- content/themes/ocooking/index.php
+++ content/themes/ocooking/index.php
@@ -1,11 +1,17 @@
 <?php
-
-if ( have_posts() ):
-    while (have_posts() ) :
+get_header();
+?>
+<main class="main">
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) :
         the_post();
-
         the_title();
         the_content();
-
     endwhile;
 endif;
+?>
+</main>
+<?php
+get_sidebar();
+get_footer();
---
+++ content/themes/ocooking/sidebar.php
@@ -0,0 +1,41 @@
+<aside class="sidebar">
+    <div class="sidebar__name">oCooking</div>
+    <div class="sidebar__baseline">Se faire plaisir tous les jours</div>
+    <div class="sidebar__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate accusantium inventore, tenetur nisi sint non, animi exercitationem excepturi at laborum facilis ipsa molestias iste incidunt rerum sed. Sunt dolore, hic exercitationem commodi animi, illum rem provident fuga dignissimos aliquam magni earum sit cupiditate fugiat necessitatibus esse tenetur beatae eligendi consectetur!</div>
+    <nav class="menu menu--vertical">
+      <ul class="menu__list">
+        <li class="menu__list__list-item"><a class="menu__list__list-item__link" href="#">Blog</a></li>
+        <li class="menu__list__list-item">
+          <a class="menu__list__list-item__link" href="#">Types de recettes</a>
+          <ul>
+            <li><a href="#">Plats de résistances</a></li>
+            <li><a href="#">En cas</a></li>
+            <li><a href="#">Petit déjeuner</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <form action="" class="recipe-proposal">
+      <fieldset>
+        <div class="form-group">
+          <label for="title">Recette</label>
+          <input class="form-control" type="text" name="title" id="title" />
+        </div>
+        <div class="form-group">
+          <label for="steps">Instructions</label>
+          <textarea class="form-control" name="steps" id="steps"></textarea>
+        </div>
+      </fieldset>
+      <fieldset>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input class="form-control" type="email" name="email" id="email" />
+        </div>
+        <div class="form-group">
+          <input type="checkbox" name="publish-agreement" id="publish-agreement" />
+          <label for="publish-agreement">Je suis d'accord pour que cette recette soit publiée sur oCooking</label>
+        </div>
+      </fieldset>
+      <button type="submit" class="btn btn-green">Proposer</button>
+    </form>
+  </aside>
