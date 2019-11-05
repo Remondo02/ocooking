7b2e6c1c6ce4c1f3d385bf8b9bc7baf0c9e5adb2
remi meullemeestre <remondo02@hotmail.com>
2019-11-05 15:50:40 +0100

    l10n (localization)

--- content/themes/ocooking/inc/setup.php
+++ content/themes/ocooking/inc/setup.php
@@ -1,10 +1,19 @@
 <?php
 
- add_action( 'after_setup_theme', 'ocooking_setup' );
+add_action( 'after_setup_theme', 'ocooking_setup' );
 
- function ocooking_setup()
- {
-     add_theme_support( 'title-tag' );
+function ocooking_setup()
+{
+    add_theme_support( 'title-tag' );
 
-     add_theme_support( 'post-thumbnails' );
- }
+    add_theme_support( 'post-thumbnails' );
+}
+
+add_action( 'after_setup_theme', 'ocooking_load_theme_textdomain' );
+function ocooking_load_theme_textdomain()
+{
+    load_theme_textdomain(
+        OCOOKING_TEXT_DOMAIN,
+        get_theme_file_path() . '/languages'
+    );
+}
